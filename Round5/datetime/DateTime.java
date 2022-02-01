7e71e09e4ce7113465f440c21794eb83b2de27d9
unknown <joni.e.koskinen@tuni.fi>
2022-02-01 13:56:11 +0200

    Date

---
+++ Round5/datetime/DateTime.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author Joni
+ */
+public class DateTime extends Date{
+    
+    private int hour;
+    private int minute;
+    private int second;
+    
+    public DateTime(int year, int month, int day, int hour, int minute,
+            int second)throws DateException{
+        super(year, month, day);
+        if(hour<0 || hour>23 || minute < 0 || minute > 59 || second < 0 || 
+                second > 59){
+            throw new DateException(String.format("Illegal time %2d:%2d:%2d",
+                    hour, minute, second));
+        }
+        else{
+            this.hour = hour;
+            this.minute = minute;
+            this.second = second;
+        }
+        
+    }
+    public int getHour(){
+        return hour;
+    }
+    public int getMinute(){
+        return minute;
+    }
+    public int getSecond(){
+        return second;
+    }
+    public String toString(){
+        String str = String.format(super.toString()+" %2d.%2d.%2d",hour,minute,second);
+        return str;
+    }
+}
