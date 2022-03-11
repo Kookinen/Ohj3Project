6fd8536f23eb3ba6f139484815e7082f40d62f9c
Joni <joni.e.koskinen@tuni.fi>
2022-03-11 09:26:10 +0200

    javadocjson package-infon muokkaus

--- Round10/javadocjson/src/main/java/fi/tuni/prog3/json/package-info.java
+++ Round10/javadocjson/src/main/java/fi/tuni/prog3/json/package-info.java
@@ -1,7 +1,4 @@
 /**
- * Sisältää  Ohjelmointi 3:n JSON-harkan luokkatoteutukset
- * <p>
- * @author Joni
- * @since 1.0
+ * Provides tools for representing data in JSON format.
  */
 package fi.tuni.prog3.json;
