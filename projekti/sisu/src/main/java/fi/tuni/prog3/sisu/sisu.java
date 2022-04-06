c7237ced1c0f2e223f9e83c4be2f94756b0878ef
Joni <joni.e.koskinen@tuni.fi>
2022-04-06 09:38:42 +0300

    Uusi alku

---
+++ projekti/sisu/src/main/java/fi/tuni/prog3/sisu/sisu.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package fi.tuni.prog3.sisu;
+
+/**
+ *
+ * @author Joni
+ */
+public class sisu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+    }
+}
